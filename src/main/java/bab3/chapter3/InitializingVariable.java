659194384c388f4731ad6dd4b1a7365f75a05111
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-07 20:24:56 +0700

    Add InitializingVariable

---
+++ src/main/java/bab3/chapter3/InitializingVariable.java
@@ -0,0 +1,8 @@
+package bab3.chapter3;
+
+public class InitializingVariable {
+    public static void main(String[] args) {
+        String 愛しています = "Aku Cinta Kamu";
+        System.out.println(愛しています);
+    }
+}
