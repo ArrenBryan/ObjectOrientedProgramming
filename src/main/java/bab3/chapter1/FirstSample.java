ece5b2a71f4bad75634387c7654fbbc82cf2d394
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-07 17:49:31 +0700

    Add comments
    Add first sample description

--- src/main/java/bab3/chapter1/FirstSample.java
+++ src/main/java/bab3/chapter1/FirstSample.java
@@ -1,7 +1,11 @@
 package bab3.chapter1;
 
+/**
+* This is the first sample program in Core Java Chapter 3
+* @version 1.01 1997-03-22
+* @author Gary Cornell
+*/
 public class FirstSample {
-
     public static void main(String[] args) {
         System.out.println("We will not use 'Hello, World!'");
     }
