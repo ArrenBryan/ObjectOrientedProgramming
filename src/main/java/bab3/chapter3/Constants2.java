c78d7a049bbfc817eb5cd465b08152c17aef6385
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-07 21:32:01 +0700

    Add Constants2

---
+++ src/main/java/bab3/chapter3/Constants2.java
@@ -0,0 +1,26 @@
+package bab3.chapter3;
+
+public class Constants2 {
+    public static void main(String[] args) {
+        System.out.println("Jumlah Mobil Awal = " + Mobil.jumlahMobil);
+        Mobil a = new Mobil();
+        System.out.println("Jumlah Mobil a = " + a.jumlahMobil);
+        Mobil b = new Mobil();
+        System.out.println("Jumlah Mobil b = " + b.jumlahMobil);
+        Mobil c = new Mobil();
+        System.out.println("Jumlah Mobil c = " + c.jumlahMobil + "\n");
+        
+        System.out.println("Jumlah Mobil a = " + a.jumlahMobil);
+        System.out.println("Jumlah Mobil b = " + b.jumlahMobil);
+        System.out.println("Jumlah Mobil c = " + c.jumlahMobil);
+        System.out.println("Jumlah Mobil Akhir = " + Mobil.jumlahMobil);
+    }
+}
+
+class Mobil {
+    public static int jumlahMobil;
+
+    Mobil() {
+        jumlahMobil++;
+    }
+}
