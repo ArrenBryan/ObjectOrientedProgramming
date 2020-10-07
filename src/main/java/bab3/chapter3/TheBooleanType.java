e7a5bb592f8ddcfa3728cfa0f2dc3ee1e26a6cd7
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-07 20:10:07 +0700

    TheBooleanType

---
+++ src/main/java/bab3/chapter3/TheBooleanType.java
@@ -0,0 +1,17 @@
+package bab3.chapter3;
+
+public class TheBooleanType {
+    public static void main(String[] args) {
+        boolean sayaBahagia = true;
+        
+        if (sayaBahagia = true)
+        {
+            System.out.println("Mamah senang");
+        }
+        
+        if (sayaBahagia = false)
+        {
+            System.out.println("Mamah tidak senang");
+        }
+    }
+}
