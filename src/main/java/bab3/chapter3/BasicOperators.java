00175ce0467d50b365edf19c836f9591d58638de
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-09 17:09:57 +0700

    Add comments

--- src/main/java/bab3/chapter3/BasicOperators.java
+++ src/main/java/bab3/chapter3/BasicOperators.java
@@ -5,17 +5,40 @@ public class BasicOperators {
         int iniInteger = 10;
         iniInteger %= 3;
         System.out.println("Integer % 3 = " + iniInteger);
-        
+
         iniInteger += (int) 3.5;
         System.out.println("Integer % 3 = " + iniInteger + "\n");
+
+        int a = 0;
+        int b = 3;
         
-        int m = 7;
-        int n = 7;
-        int a = 2 * ++m;
-        int b = 2 * n++;
-        System.out.println("m = " + m);
-        System.out.println("n = " + n);
+        /* 
+         * Langkah-langkah yang dilakukan:
+         * 1. a = 0, b = 3,
+         * 2. b = b + 2, sehingga b = 5
+         * 3. b = b + a, sehingga b = 5,
+         * 4. a = a + 1, sehingga a = 1,
+         * 5. b = b + a, sehingga b = 6,
+         * 6. a = a + 1, sehingga a = 2.
+         */
+        b += 2 + a++ + a++;
         System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("b = " + b + "\n");
+        
+        b = 3;
+        a = 0;
+        
+        /* 
+         * Langkah-langkah yang dilakukan:
+         * 1. a = 0, b = 3,
+         * 2. b = b + 2, sehingga b = 5
+         * 4. a = a + 1, sehingga a = 1,
+         * 5. b = b + a, sehingga b = 6,
+         * 6. a = a + 1, sehingga a = 2.
+         * 3. b = b + a, sehingga b = 8,
+         */
+        b += 2 + ++a + ++a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b + "\n");
     }
 }
