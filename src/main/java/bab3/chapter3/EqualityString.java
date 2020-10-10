d8340494a9d36302e0c3835797df18ff39e20960
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-10 21:56:03 +0700

    Add EqualityString

---
+++ src/main/java/bab3/chapter3/EqualityString.java
@@ -0,0 +1,11 @@
+package bab3.chapter3;
+
+public class EqualityString {
+    public static void main(String[] args) {
+        String iniString1 = "Ayam";
+        String iniString2 = "ayam";
+        
+        System.out.println(iniString1.equals(iniString2));
+        System.out.println(iniString1.equalsIgnoreCase(iniString2));
+    }
+}
