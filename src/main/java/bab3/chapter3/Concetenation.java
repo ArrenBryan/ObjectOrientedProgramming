b9ccd38b5907fd6de821f263ff4ceb155ee500f4
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-10 11:48:47 +0700

    Add Concetenation

---
+++ src/main/java/bab3/chapter3/Concetenation.java
@@ -0,0 +1,14 @@
+package bab3.chapter3;
+
+public class Concetenation {
+
+    public static void main(String[] args) {
+        String iniString = String.join(" ", "Bryan", "Azriel", "Parulian");
+        System.out.println(iniString);
+
+        String iniString2 = String.join(" -> ", "Wake-up", "Eat", "Game",
+                "Sleep", "Wake-up");
+        System.out.println(iniString2);
+    }
+
+}
