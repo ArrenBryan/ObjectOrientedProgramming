aa0959405d9dcc3b73030e35e78ab7c1363ab62e
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-10 22:57:10 +0700

    Add CodePoints

---
+++ src/main/java/bab3/chapter3/CodePoints.java
@@ -0,0 +1,20 @@
+package bab3.chapter3;
+
+public class CodePoints {
+
+    public static void main(String[] args) {
+        String sentence = "𝑖 is set of octonions";
+        System.out.println(sentence + "\n");
+
+        for (int i = 0; i < sentence.length();) {
+            int cp = sentence.codePointAt(i);
+            if (Character.isSupplementaryCodePoint(cp)) {
+                System.out.println("" + sentence.charAt(i) + sentence.charAt(i+1));
+                i += 2;
+            } else {
+                System.out.println(sentence.charAt(i));
+                i++;
+            }
+        }
+    }
+}
