e42557c7c01713b405344b472fb07de882c6b0b5
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-10 11:22:49 +0700

    Add Substrings

---
+++ src/main/java/bab3/chapter3/Substrings.java
@@ -0,0 +1,11 @@
+package bab3.chapter3;
+
+public class Substrings {
+
+    public static void main(String[] args) {
+        String iniString = "BryanAzrielP";
+        String iniString2 = iniString.substring(0, 5);
+        System.out.println("iniString = " + iniString);
+        System.out.println("iniString2 = " + iniString2);
+    }
+}
