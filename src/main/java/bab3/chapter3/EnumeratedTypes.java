8ecb73a82e4e883ae57b2711b8fd155f669af9ee
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-10 18:19:27 +0700

    Add EnumeratedTypes

---
+++ src/main/java/bab3/chapter3/EnumeratedTypes.java
@@ -0,0 +1,31 @@
+package bab3.chapter3;
+
+public class EnumeratedTypes {
+
+    public enum Hari {
+        SENIN(1),
+        SELASA(2),
+        RABU(3),
+        KAMIS(4),
+        JUMAT(5),
+        SABTU(6),
+        MINGGU(7);
+        
+        public int inInteger;
+        
+        Hari(int i) {
+            this.inInteger = i;
+        }
+    }
+
+    public static void main(String[] args) {
+        Hari hariLibur = Hari.MINGGU;
+        System.out.println("Hari libur adalah " + hariLibur);
+        
+        Hari hariMauLibur = Hari.SABTU;
+        if (hariMauLibur.inInteger == 6)
+        {
+            System.out.println("Hari mau libur adalah " + hariMauLibur);
+        }
+    }
+}
