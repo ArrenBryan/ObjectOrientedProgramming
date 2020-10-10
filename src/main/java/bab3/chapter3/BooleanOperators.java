9513eebee9c719706499e74ad4f8bfd3f104cee3
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-10 10:24:14 +0700

    Add BooleanOperators

---
+++ src/main/java/bab3/chapter3/BooleanOperators.java
@@ -0,0 +1,13 @@
+package bab3.chapter3;
+
+public class BooleanOperators {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 15;
+
+        int max = x > y ? x : y;
+        System.out.println("max = " + max);
+    }
+
+}
