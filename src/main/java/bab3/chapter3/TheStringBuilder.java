2c5b95a7ca234e21cef4122b5154a00371e6dca3
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-11 09:33:58 +0700

    Add TheStringBuilder

---
+++ src/main/java/bab3/chapter3/TheStringBuilder.java
@@ -0,0 +1,26 @@
+package bab3.chapter3;
+
+public class TheStringBuilder {
+
+    public static void main(String[] args) {
+        String iniString = "";
+        long start;
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            iniString += "hello";
+        }
+
+        System.out.println("String's time: " + (System.currentTimeMillis() - start));
+
+        System.out.println();
+
+        start = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10000; i++) {
+            sb.append("hello");
+        }
+
+        System.out.println("StringBuilder's time: " + (System.currentTimeMillis() - start));
+    }
+}
