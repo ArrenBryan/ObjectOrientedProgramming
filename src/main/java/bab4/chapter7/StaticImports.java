154bf2fbe8545da55c7681ff28c15154a37b628f
ArrenBryan <bryan.azriel.tif19@polban.ac.id>
2020-10-31 18:28:05 +0700

    Add StaticImports

---
+++ src/main/java/bab4/chapter7/StaticImports.java
@@ -0,0 +1,13 @@
+package bab4.chapter7;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+public class StaticImports {
+
+    public static void main(String[] args) {
+        out.println("Hello World");
+        out.println(sin(toRadians(60)));
+    }
+
+}
